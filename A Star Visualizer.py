a3df77574331ccea48d593f27b898a1a66901dbf
ns8 <73150842+ns8wastaken@users.noreply.github.com>
2024-04-08 20:48:23 -0500

    Add files via upload

---
+++ A Star Visualizer.py
@@ -0,0 +1,206 @@
+import pygame, time
+import json, pickle
+
+from AStar import AStarSolver
+
+class Visualizer:
+    def __init__(
+            self,
+            fps: int,
+            maze_sizeX:int,
+            maze_sizeY: int,
+            tileSize: int,
+            start: tuple[int, int],
+            end: tuple[int, int],
+            outlineWidth: int = 3,
+            debug: bool = False
+        ):
+
+        pygame.init()
+        pygame.display.set_caption('A* Visualizer')
+        pygame.display.set_icon(pygame.image.load('A Star.png'))
+        self.fps = fps
+        self.frame_count = 0
+
+        # Setup
+        self.tileSize = tileSize
+        self.outlineWidth = outlineWidth
+
+        self.maze_sizeX = maze_sizeX
+        self.maze_sizeY = maze_sizeY
+
+        self.screen = pygame.display.set_mode((self.maze_sizeX * self.tileSize, self.maze_sizeY * self.tileSize))
+
+        # Maze init
+        self.maze: dict[tuple[int, int], bool] = {(x, y): False for y in range(self.maze_sizeY) for x in range(self.maze_sizeX)}
+
+        self.solver = AStarSolver(self.maze, start, end)
+
+        self.font = pygame.font.SysFont('jetbrainsmonoregular', 20)
+
+        # Pressed keys
+        self.keys = {
+            'left_mouse_held': False,
+            'right_mouse_held': False
+        }
+
+        self.debug = debug
+
+    def save_maze(self):
+        with open('maze.json', 'w') as f:
+            json.dump({f'{key[0]};{key[1]}': val for key, val in self.maze.items()}, f)
+
+    def load_maze(self):
+        with open('maze.json', 'r') as f:
+            self.maze.clear()
+            for key, val in json.load(f).items():
+                key = key.split(';')
+                key = (int(key[0]), int(key[1]))
+                self.maze[key] = val
+
+    def draw_solver_open(self):
+        for x, y in self.solver.open:
+            pygame.draw.rect(self.screen, (42, 130, 196), (x * self.tileSize, y * self.tileSize, self.tileSize, self.tileSize))
+
+    def draw_solver_closed(self):
+        for x, y in self.solver.closed:
+            pygame.draw.rect(self.screen, (49, 4, 107), (x * self.tileSize, y * self.tileSize, self.tileSize, self.tileSize))
+
+    def draw_solver_path(self):
+        for coords in self.solver.path:
+            pygame.draw.rect(self.screen, (140, 27, 106), (coords[0] * self.tileSize, coords[1] * self.tileSize, self.tileSize, self.tileSize))
+
+    def draw_maze(self):
+        self.draw_solver_closed()
+        self.draw_solver_open()
+        self.draw_solver_path()
+
+        pygame.draw.rect(self.screen, (0, 255, 0), (self.solver.start[0] * self.tileSize, self.solver.start[1] * self.tileSize, self.tileSize, self.tileSize))
+        pygame.draw.rect(self.screen, (255, 0, 0), (self.solver.end[0] * self.tileSize, self.solver.end[1] * self.tileSize, self.tileSize, self.tileSize))
+
+        for coords, is_wall in self.maze.items():
+            if is_wall:
+                # Draw walls
+                pygame.draw.rect(self.screen, (0, 0, 0), (coords[0] * self.tileSize, coords[1] * self.tileSize, self.tileSize, self.tileSize))
+
+            else:
+                # Draw tile outlines
+                pygame.draw.rect(self.screen, (0, 0, 0), (coords[0] * self.tileSize, coords[1] * self.tileSize, self.tileSize, self.tileSize), width=self.outlineWidth)
+
+    def run(self):
+        clock = pygame.time.Clock()
+
+        _previous_time = time.time()
+
+        start = False
+
+        while True:
+            clock.tick(self.fps)
+
+            dt = time.time() - _previous_time
+            _previous_time = time.time()
+
+            self.frame_count += 1
+
+            mx, my = pygame.mouse.get_pos()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+
+                if event.type == pygame.KEYDOWN:
+                    if (event.key == pygame.K_q and pygame.key.get_mods() & pygame.KMOD_CTRL) or event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        return
+
+                    if event.key == pygame.K_SPACE:
+                        start = True
+
+                    if event.key == pygame.K_BACKSPACE:
+                        self.solver.reset()
+                        start = False
+
+                    if event.key == pygame.K_s:
+                        self.save_maze()
+
+                    if event.key == pygame.K_o:
+                        self.load_maze()
+
+                    if event.key == pygame.K_1:
+                        tile_hover = (mx // self.tileSize, my // self.tileSize)
+                        self.solver.update_start(tile_hover)
+
+                    if event.key == pygame.K_2:
+                        tile_hover = (mx // self.tileSize, my // self.tileSize)
+                        self.solver.end = tile_hover
+
+                if not start:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            self.keys['left_mouse_held'] = True
+
+                        if event.button == 3:
+                            self.keys['right_mouse_held'] = True
+
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        if event.button == 1:
+                            self.keys['left_mouse_held'] = False
+                            self.solver.update_maze(self.maze)
+
+                        if event.button == 3:
+                            self.keys['right_mouse_held'] = False
+                            self.solver.update_maze(self.maze)
+
+            if self.keys['left_mouse_held']:
+                mouse_tile_clicked = (mx // self.tileSize, my // self.tileSize)
+                self.maze[mouse_tile_clicked] = True
+            elif self.keys['right_mouse_held']:
+                mouse_tile_clicked = (mx // self.tileSize, my // self.tileSize)
+                self.maze[mouse_tile_clicked] = False
+
+            self.screen.fill((255, 255, 255))
+
+            self.draw_maze()
+
+            if start and self.frame_count % 60 == 0:
+                if not self.solver.done1:
+                    self.solver.choose_next_tile(diagonals = True)
+
+                elif not self.solver.done2:
+                    self.solver.get_path(self.solver.end)
+
+            if self.debug:
+                for coords in self.solver.open | self.solver.closed:
+                    g, h = self.solver.get_g(coords), self.solver.get_h('manhattan', coords)
+                    self.screen.blit(self.font.render(
+                        str(round(g + h)),
+                        True, (255, 0, 255)
+                        ), (coords[0] * self.tileSize, coords[1] * self.tileSize)
+                    )
+
+                    self.screen.blit(self.font.render(
+                        str(round(g)),
+                        True, (255, 0, 255)
+                        ), (coords[0] * self.tileSize, coords[1] * self.tileSize + 25)
+                    )
+
+                    self.screen.blit(self.font.render(
+                        str(round(h)),
+                        True, (255, 0, 255)
+                        ), (coords[0] * self.tileSize, coords[1] * self.tileSize + 50)
+                    )
+
+            pygame.display.update()
+
+if __name__ == '__main__':
+    Visualizer(
+        fps          = 60,
+        maze_sizeX   = 8,
+        maze_sizeY   = 6,
+        tileSize     = 80,
+        start        = (5, 4),
+        end          = (2, 1),
+        outlineWidth = 3,
+        debug        = True
+    ).run()
---
+++ AStar.py
@@ -0,0 +1,157 @@
+from typing import Literal
+from math import sqrt
+import random
+
+def _round(x: float):
+    return int(x + 0.5)
+
+class AStarSolver:
+    def __init__(self, maze: dict[tuple[int, int], bool], start: tuple[int, int], end: tuple[int, int]):
+        self.maze = maze
+
+        self.start = start
+        self.end = end
+
+        self.open: dict[tuple[int, int], tuple[int, int]] = {self.start: self.start}
+        self.closed: dict[tuple[int, int], tuple[int, int]] = dict()
+        self.path = []
+
+        self.done1 = False
+        self.done2 = False
+
+        self.cost_orthogonal = 10
+        self.cost_diagonal = 14
+
+    def update_maze(self, maze: dict[tuple[int, int], bool]):
+        self.maze = maze
+
+    def update_start(self, start: tuple[int, int]):
+        self.start = start
+        self.open = {self.start: self.start}
+
+    def reset(self):
+        self.open = {self.start: self.start}
+        self.closed.clear()
+        self.path.clear()
+        self.done1 = False
+        self.done2 = False
+
+    def get_path(self, coords: tuple[int, int]):
+        if (next_coords := self.closed[coords]) != coords:
+            self.path.append(coords)
+            self.get_path(next_coords)
+
+        else:
+            self.path.append(coords)
+
+        self.done2 = True
+
+    def get_neighbors(self, coords: tuple[int, int], diagonals: bool):
+        neighbor_offsets = (
+            (-1, -1), (0, -1), (1, -1),
+            (1, 0), (1, 1), (0, 1),
+            (-1, 1), (-1, 0)
+        ) if diagonals else (
+            (0, -1), (1, 0), (0, 1), (-1, 0)
+        )
+
+        for dx, dy in neighbor_offsets:
+            neighbor_pos = (coords[0] + dx, coords[1] + dy)
+
+            # Skip if the neighbor is opened or closed already
+            if neighbor_pos in self.closed:
+                continue
+
+            elif neighbor_pos in self.open:
+                if self.get_g(neighbor_pos) > self.get_g(coords) + (self.cost_diagonal if self.is_diagonal(coords, neighbor_pos) else self.cost_orthogonal):
+                    self.open[neighbor_pos] = coords
+
+            # Yield the neighbor's pos if it is in the maze and not a wall
+            elif neighbor_pos in self.maze and self.maze[neighbor_pos] == False:
+                yield (neighbor_pos, coords)
+
+    def is_diagonal(self, _from: tuple[int, int], _to: tuple[int, int]):
+        return (_to[0] - _from[0] != 0) and (_to[1] - _from[1] != 0)
+
+    def get_h(self, mode: Literal['manhattan', 'diagonal', 'euclidian', 'dijkstra'], coords: tuple[int, int]) -> float:
+        match mode:
+            case 'manhattan':
+                return (abs(coords[0] - self.end[0]) + abs(coords[1] - self.end[1])) * self.cost_orthogonal
+
+            case 'diagonal':
+                dx = abs(coords[0] - self.end[0])
+                dy = abs(coords[1] - self.end[1])
+                return ((dx + dy) + (1.4 - 2) * min(dx, dy)) * self.cost_orthogonal
+
+            case 'euclidian':
+                return sqrt((self.end[0] - coords[0])**2 + (self.end[1] - coords[1])**2) * self.cost_orthogonal
+
+            case 'dijkstra':
+                return 0
+
+    def get_g(self, coords: tuple[int, int]) -> float:
+        cost = 0
+
+        current_coords = coords
+        if current_coords in self.open: next_coords = self.open[current_coords]
+        else:                           next_coords = self.closed[current_coords]
+
+        while current_coords != next_coords:
+            next_coords = current_coords
+
+            if current_coords in self.open:
+                current_coords = self.open[current_coords]
+
+            else:
+                current_coords = self.closed[current_coords]
+
+            cost += self.cost_diagonal if self.is_diagonal(current_coords, next_coords) else self.cost_orthogonal
+
+        return cost
+
+    def choose_next_tile(self, diagonals: bool):
+        if self.end in self.closed:
+            self.done1 = True
+            return
+
+        min_f = float('inf')
+        min_h = float('inf')
+        min_coords_f: list[tuple[int, int]] = []
+        coords_h: list[tuple[int, int]] = []
+
+        _f = []
+        for coords in self.open:
+            g = _round(self.get_g(coords))
+            h = _round(self.get_h('manhattan', coords))
+            f = g + h
+            _f.append(f)
+
+            if f < min_f:
+                min_f = f
+                min_coords_f = [coords]
+
+                if h < min_h:
+                    min_h = h
+                    coords_h = [coords]
+
+                elif h == min_h:
+                    coords_h.append(coords)
+
+            elif f == min_f:
+                min_coords_f.append(coords)
+
+                if h < min_h:
+                    min_h = h
+                    coords_h = [coords]
+
+                elif h == min_h:
+                    coords_h.append(coords)
+
+        if len(min_coords_f) == 1:
+            self.closed.update({min_coords_f[0]: self.open.pop(min_coords_f[0])})
+            self.open.update(self.get_neighbors(min_coords_f[0], diagonals))
+
+        else:
+            coords = random.choice(coords_h)
+            self.closed.update({coords: self.open.pop(coords)})
+            self.open.update(self.get_neighbors(coords, diagonals))
